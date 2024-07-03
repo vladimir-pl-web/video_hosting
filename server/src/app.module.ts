9c7ee64ef1298d5f655a32d9af57035089de1a33
Vladimirs Plotnikovs <vladimirplotnikov371@gmail.com>
2024-07-03 19:48:26 +0300

    backend init endpoints

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
+import { CommentModule } from './comment/comment.module';
+import { AuthModule } from './auth/auth.module';
+
 
 @Module({
-  imports: [],
+  imports: [UserModule, VideoModule, CommentModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
